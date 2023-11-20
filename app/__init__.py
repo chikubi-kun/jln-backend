07d9aafc229085a1866e70469bdfe957f998d151
Chikubi-Kun <raiseyourtail@gmail.com>
2023-11-20 10:29:05 +0700

    feat: update app factory

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,17 @@ from sanic import Sanic
 
 
 def create_app() -> Sanic:
-    from . import api
+    from . import api, config, db
 
     app = Sanic(__name__)
 
+    # Load config
+    config.init(app)
+
+    # Load database
+    db.init(app)
+
+    # # Load API
     api.init(app)
 
     return app
