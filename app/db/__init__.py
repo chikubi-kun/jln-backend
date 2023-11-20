7c5b60628270a623010eb563e76e33620e6eff40
Chikubi-Kun <raiseyourtail@gmail.com>
2023-11-20 16:36:48 +0700

    feat: update db init

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,6 +1,9 @@
 from .factory import Database
+from .factory import Migration
 
 db = Database()
+migrate = Migration()
 
 def init(app):
-    db.init_db(app)
+    db.init_app(app)
+    migrate.init_app(app, directory=app.config.get("DB_MIGRATIONS_DIR"))
